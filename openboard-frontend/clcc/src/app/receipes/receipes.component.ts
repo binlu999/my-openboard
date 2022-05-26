7b2ccd39441eda77bb5b4c6035760dd0907cc79b
Lu, Bin <blu@wm.com>
2022-05-26 02:26:53 -0500

    service

--- openboard-frontend/clcc/src/app/receipes/receipes.component.ts
+++ openboard-frontend/clcc/src/app/receipes/receipes.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Receipe } from './receipe.model';
+import { ReceipeService } from './receipe.service';
 
 @Component({
   selector: 'app-receipes',
   templateUrl: './receipes.component.html',
-  styleUrls: ['./receipes.component.css']
+  styleUrls: ['./receipes.component.css'],
+  providers:[ReceipeService]
 })
 export class ReceipesComponent implements OnInit {
   selectedReceipe:Receipe;
