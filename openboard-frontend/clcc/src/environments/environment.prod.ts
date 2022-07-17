ed7929bbae6c8ffabbf5ce3487e80772e0ae6018
Lu, Bin <blu@wm.com>
2022-07-17 18:07:53 -0500

    add url

--- openboard-frontend/clcc/src/environments/environment.prod.ts
+++ openboard-frontend/clcc/src/environments/environment.prod.ts
@@ -3,5 +3,6 @@ export const environment = {
   AUTH_SIGNUP_URL:'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=',
   AUTH_SIGNIN_URL:'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=',
   AUTH_API_KEY:'AIzaSyCHZLEmFGsPJ_ZcMOpxZW0k0mWJUUsjlXU',
-  AUTH_SEC_TOKEN_NAME:'clcc-openboard-user-data'
+  AUTH_SEC_TOKEN_NAME:'clcc-openboard-user-data',
+  RECEIPE_STORE_URL:'https://receipe-book-6c464-default-rtdb.firebaseio.com/receipes.json',
 };
